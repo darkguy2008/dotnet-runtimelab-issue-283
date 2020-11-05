17caad84d7d859e18c271c26ef4181f55f94ddd1
DARKGuy <dark.guy.2008@gmail.com>
2020-11-05 12:56:14 -0600

    Remove old logfiles. Found the bug! It's in Pomelo.EntityFrameworkCore.MySql :)

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace app
     {
         static void Main(string[] args)
         {
+            // Pomelo.EntityFrameworkCore.MySql causes the bug!
+            Pomelo.EntityFrameworkCore.MySql.Storage.CharSet c = new Pomelo.EntityFrameworkCore.MySql.Storage.CharSet("aaa", 123);
             Console.WriteLine("Hello World!");
         }
     }
